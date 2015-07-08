23a81d2cc651860d7a1fd5c2c1cc05da334eba3b
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2015-07-08 17:37:23 -0400

    opto

--- assets/js/script.map.js
+++ assets/js/script.map.js
@@ -1,7 +1,7 @@
 app.map = {
 	init: function() {
 		var para = "p.sirlanka,p.mexico,p.usa,p.chile,p.saudiarabia,p.oman,p.kuwait,p.pakistan,p.india,p.netherlands,p.switerland,p.malaysia,p.philippines,p.southkorea,p.australia,p.newzeland,p.signapor,p.sirlanka,p.nigeria,p.egypt,p.ireland,p.unitedkingdom",
-			map = "#sirlanka_svg,#mexico_svg,#usa_svg,#chile_svg,#saudiarabia_svg,#oman_svg,#kuwait_svg,#pakistan_svg,#india_svg,#netherlands_svg,#switerland_svg,#malaysia_svg,#philippines_svg,#southkorea_svg,#australia_svg,#newzeland_svg,#signapor_svg,#sirlanka_svg,#nigeria_svg,#egypt_svg,#ireland_svg,#unitedkingdom_svg";
+			map = "#mexico_svg,#unitedkingdom_svg,#ireland_svg,#egypt_svg,#nigeria_svg,#usa_svg,#chile_svg,#sirlanka_svg,#signapor_svg,#newzeland_svg,#australia_svg,#southkorea_svg,#philippines_svg,#malaysia_svg,#switerland_svg,#netherlands_svg,#india_svg,#pakistan_svg,#kuwait_svg,#oman_svg,#saudiarabia_svg,#canada_svg,#colombia_svg,#guyana_svg,#germany_svg,#iran_svg,#israel_svg";
 
 		$(map).click(app.map.countryclick).mouseover(app.map.countryclick); //.mouseout(app.map.nocountry)
 		$(para).click(app.map.countryclick).mouseover(app.map.countryclick);
