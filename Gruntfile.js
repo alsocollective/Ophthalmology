3e8da7c06fc9bfae5eb148edabdfca08b201c921
adminlocal <adminlocal@Bohdans-MacBook-Pro.local>
2015-11-11 13:20:46 -0500

    updates the meta tags

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-var mozjpeg = require('imagemin-mozjpeg');
+// var mozjpeg = require('imagemin-mozjpeg');
 
 module.exports = function(grunt) {
 	grunt.initConfig({
@@ -51,81 +51,80 @@ module.exports = function(grunt) {
 		},
 
 
-		responsive_images: {
-			first: {
-				options: {
-					engine: 'gm',
-					newFilesOnly: true,
-					upscale: false,
-					sizes: [{
-						name: 'tiny',
-						width: 640
-					}, {
-						name: 'small',
-						width: 1280
-					}, {
-						name: 'medium',
-						width: 2048,
-					}, {
-						name: 'large',
-						width: 4096,
-					}, {
-						name: 'original',
-						width: "100%",
-					}],
-				},
-				files: [{
-					expand: true,
-					src: ['**.{jpg,gif,png}'],
-					cwd: 'assets/pic/',
-					dest: 'assets/temp/'
-				}]
+		// responsive_images: {
+		// 	first: {
+		// 		options: {
+		// 			engine: 'gm',
+		// 			newFilesOnly: true,
+		// 			upscale: false,
+		// 			sizes: [{
+		// 				name: 'tiny',
+		// 				width: 640
+		// 			}, {
+		// 				name: 'small',
+		// 				width: 1280
+		// 			}, {
+		// 				name: 'medium',
+		// 				width: 2048,
+		// 			}, {
+		// 				name: 'large',
+		// 				width: 4096,
+		// 			}, {
+		// 				name: 'original',
+		// 				width: "100%",
+		// 			}],
+		// 		},
+		// 		files: [{
+		// 			expand: true,
+		// 			src: ['**.{jpg,gif,png}'],
+		// 			cwd: 'assets/pic/',
+		// 			dest: 'assets/temp/'
+		// 		}]
 
-			},
-			second: {
-				options: {
-					engine: 'gm',
-					newFilesOnly: true,
-					upscale: false,
-					sizes: [{
-						name: 'o',
-						width: '50%',
-						suffix: "@1x",
-					}, {
-						name: 'r',
-						width: '100%',
-						suffix: "@2x",
-					}],
-				},
+		// 	},
+		// 	second: {
+		// 		options: {
+		// 			engine: 'gm',
+		// 			newFilesOnly: true,
+		// 			upscale: false,
+		// 			sizes: [{
+		// 				name: 'o',
+		// 				width: '50%',
+		// 				suffix: "@1x",
+		// 			}, {
+		// 				name: 'r',
+		// 				width: '100%',
+		// 				suffix: "@2x",
+		// 			}],
+		// 		},
 
-				files: [{
-					expand: true,
-					src: ['**.{jpg,gif,png}'],
-					cwd: 'assets/temp/',
-					dest: 'public_html/assets/pic/'
-				}]
+		// 		files: [{
+		// 			expand: true,
+		// 			src: ['**.{jpg,gif,png}'],
+		// 			cwd: 'assets/temp/',
+		// 			dest: 'public_html/assets/pic/'
+		// 		}]
 
-			}
-		},
+		// 	}
+		// },
 
-		imagemin: {
-			dynamic: { // Another target 
-				options: { // Target options 
-					optimizationLevel: 3,
-					svgoPlugins: [{
-						removeViewBox: false
-					}],
-					use: [mozjpeg()]
-				},
-				files: [{
-					expand: true, // Enable dynamic expansion 
-					cwd: 'public_html/assets/pic/', // Src matches are relative to this path 
-					src: ['**/*.{png,jpg,gif}'], // Actual patterns to match 
-					dest: 'public_html/assets/pic/' // Destination path prefix 
-				}]
-			}
-		},
-
+		// imagemin: {
+		// 	dynamic: { // Another target 
+		// 		options: { // Target options 
+		// 			optimizationLevel: 3,
+		// 			svgoPlugins: [{
+		// 				removeViewBox: false
+		// 			}],
+		// 			use: [mozjpeg()]
+		// 		},
+		// 		files: [{
+		// 			expand: true, // Enable dynamic expansion 
+		// 			cwd: 'public_html/assets/pic/', // Src matches are relative to this path 
+		// 			src: ['**/*.{png,jpg,gif}'], // Actual patterns to match 
+		// 			dest: 'public_html/assets/pic/' // Destination path prefix 
+		// 		}]
+		// 	}
+		// },
 
 		includes: {
 			// reference 
@@ -170,16 +169,16 @@ module.exports = function(grunt) {
 			js: {
 				files: 'assets/**/*.js',
 				tasks: ['uglify']
-			},
-			img: {
-				files: 'assets/pic/**/*.jpg',
-				tasks: ['responsive_images:first', 'responsive_images:second'] //, 'imagemin']
-			}
+			} //,
+			// img: {
+			// 	files: 'assets/pic/**/*.jpg',
+			// 	tasks: ['responsive_images:first', 'responsive_images:second'] //, 'imagemin']
+			// }
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-responsive-images');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	// grunt.loadNpmTasks('grunt-responsive-images');
+	// grunt.loadNpmTasks('grunt-contrib-imagemin');
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
